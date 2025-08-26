5d3d5cf0a549f15f87f11382e621dcd51bbdfdbe
Juli Delos <julidelos@gmail.com>
2025-08-26 10:35:49 +0200

    Second diagram is set

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,38 +3,41 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import keystatic from '@keystatic/astro';
-import react from '@astrojs/react';
-import netlify from '@astrojs/netlify';
+import keystatic from "@keystatic/astro";
+import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  output: 'server',
+  output: "server",
   adapter: netlify(),
   integrations: [mdx(), sitemap(), icon(), react(), keystatic()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        'lodash': 'lodash-es'
-      }
+        lodash: "lodash-es",
+      },
     },
     ssr: {
-      noExternal: ['@keystatic/core', '@keystatic/astro', 'lodash-es']
+      noExternal: ["@keystatic/core", "@keystatic/astro", "lodash-es"],
     },
     optimizeDeps: {
-      include: ['lodash-es']
+      include: ["lodash-es"],
     },
+    server: { 
+      port: 4321,
+      allowedHosts: [".ngrok-free.app"] },
     build: {
       minify: false,
-      target: 'esnext',
+      target: "esnext",
       rollupOptions: {
         onwarn(warning, warn) {
-          if (warning.code === 'THIS_IS_UNDEFINED') return;
+          if (warning.code === "THIS_IS_UNDEFINED") return;
           warn(warning);
-        }
-      }
-    }
+        },
+      },
+    },
   },
 });
